c9e07aef5cd19926eb297f59bc1e896f7761edfb
matt <mattsnaccc@gmail.com>
2021-09-13 11:02:08 -0700

    feat: add log

--- launcher2.sh
+++ launcher2.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # launcher2.sh
 
-screen -dm bash -c 'cd /home/pi/clock && git pull && npm i && npm run dev'
+screen -dm bash -c 'cd /home/pi/clock && git pull && npm i && npm run dev > log.txt'
 launched=false
 
 while [ "$launched" = false ]; do
