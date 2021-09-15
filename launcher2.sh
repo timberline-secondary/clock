7f8bbe1451459e74c7b5b8b52a2758745cba9e6e
matt <mattsnaccc@gmail.com>
2021-09-15 11:20:50 -0700

    fix: revert

--- launcher2.sh
+++ launcher2.sh
@@ -2,8 +2,7 @@
 # launcher2.sh
 
 screen -dm bash -c 'cd /home/pi/clock && git pull && npm i | tee /home/pi/clock/log.txt && npm run dev  | tee /home/pi/clock/log.txt'
-export DISPLAY=:0
-lxterminal --geometry=1920x1080 -e "screen -r"
+
 
 launched=false
 
@@ -12,6 +11,8 @@ while [ "$launched" = false ]; do
     echo "Not in use!"
   else
     launched=true
+
+    export DISPLAY=:0
 
     chromium-browser --start-fullscreen --incognito --app  http://0.0.0.0:3000
   fi
