0c394268368d3ed74e596def5c55bb663cac930b
matt <mattsnaccc@gmail.com>
2021-11-24 13:37:52 -0800

    feat: new flex

--- components/schedule.js
+++ components/schedule.js
@@ -1,4 +1,6 @@
 export default function Selection() {
-	/*please count from 0 */
-	return "2"; // edit this for the schedule you want displayed corresponding with the number in the json file
+	/* please count from 0, the conditional statement determines if the day is currently wednesday, true means it is. */
+	return new Date().getDay() === 3 ? "3" // this is the  flex schedule
+	: "2"; // edit this for the schedule you want displayed corresponding with the number in the json file
+
 }
