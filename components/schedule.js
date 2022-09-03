e241296842245c60e1dd767d0a900241e8c6aca6
Tylere Couture <tylerecouture@users.noreply.github.com>
2022-09-03 14:54:13 -0700

    Update schedule.js

--- components/schedule.js
+++ components/schedule.js
@@ -1,6 +1,6 @@
 export default function Selection() {
 	/* please count from 0, the conditional statement determines if the day is currently wednesday, true means it is. */
-	return new Date().getDay() === 3 ? "3" // this is the  flex schedule
-	: "2"; // edit this for the schedule you want displayed corresponding with the number in the json file
+	return new Date().getDay() === 3 ? "1" // this is the  flex schedule
+	: "0"; // edit this for the schedule you want displayed corresponding with the number in the json file
 
 }
